4020d5e0ed236ab383b11f4814e79941acd96bf5
Assad Isah <nottherealalanturing@gmail.com>
2022-09-18 21:11:41 +0100

    feat: add delete feature

--- linkedlist/linkedlist.rb
+++ linkedlist/linkedlist.rb
@@ -12,8 +12,11 @@ end
 
 
 class LinkedList
+    attr_reader :size
+
     def initialize(head = nil)
         @head = head
+        @size = 0
     end
     
     def list_length
@@ -37,33 +40,90 @@ class LinkedList
         else
             @head = newNode
         end
+        @size += 1
         newNode.data
     end
 
     def insertAtEnd(data)
-        newNode = Node.new data
-        currentNode = @head
-        while(currentNode.next != nil)
-            currentNode = currentNode.next
+        if @head == nil
+            insertAtBegining(data) 
+        else
+            newNode = Node.new data
+            currentNode = @head
+            while(currentNode.next != nil)
+                currentNode = currentNode.next
+            end
+            currentNode.next = newNode
+            @size += 1
+            newNode.data
         end
-        currentNode.next = newNode
-        newNode.data
     end
 
     def insertAtPos(data, pos)
-        newNode = Node.new data
-        tempNode = nil
-        currentNode = @head
-        count = 1;
-        while(currentNode != nil && count < pos)
-            tempNode = currentNode
-            currentNode = currentNode.next
-            count += 1
+        if @head == nil
+            insertAtBegining(data) 
+        else
+            newNode = Node.new data
+            tempNode = nil
+            currentNode = @head
+            count = 1;
+            while(currentNode != nil && count < pos)
+                tempNode = currentNode
+                currentNode = currentNode.next
+                count += 1
+            end
+            tempNode.next = newNode
+            newNode.next = currentNode
+            @size += 1
+            newNode.data
         end
-        tempNode.next = newNode
-        newNode.next = currentNode
-        newNode.data
     end
+
+    def delete_first
+        currentNode = @head
+        if (currentNode != nil)
+            @head = currentNode.next
+            @size -= 1
+            currentNode
+        end
+    end
+
+    def delete_last
+        currentNode = @head
+        last_node = currentNode.next
+        while (last_node.next != nil)
+            currentNode = last_node
+            last_node = last_node.next
+        end
+        currentNode.next = nil
+        @size -= 1
+        last_node
+    end
+
+    def delete_pos(pos)
+        if (pos > @size)
+            puts "position doesn't exist"
+        elsif (pos == 1)
+            delete_first
+        else
+            count = 1
+            currentNode = @head
+            last_node = nil
+            while ((currentNode != nil) && (count < pos))
+                count += 1
+                last_node = currentNode
+                currentNode = currentNode.next
+            end
+            last_node.next = currentNode.next
+            @size -= 1
+            currentNode
+        end
+    end
+
+    def delete_list
+        @head = nil
+    end
+
 
     def print_list
         currentNode = @head
@@ -74,14 +134,20 @@ class LinkedList
     end
 
 
+
 end
 
-#binding.pry
-
 list = LinkedList.new
+list.insertAtEnd(1)
+list.insertAtEnd(2)
+list.insertAtEnd(3)
+list.insertAtEnd(4)
+binding.pry
 
-list.insertAtBegining(1) #[1]
-list.insertAtEnd(2) #[1,2]
+
+#list.insertAtBegining(1) #[1]
+#list.insertAtEnd(2) #[1,2]
+
 list.insertAtPos(4,2) #[1,4,2]
 
 list.print_list
