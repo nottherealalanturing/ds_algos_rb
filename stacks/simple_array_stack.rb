6df84955126463e624b16c594e6102b7c072a63f
Assad Isah <nottherealalanturing@gmail.com>
2022-11-12 12:36:36 +0100

    stacks

--- stacks/simple_array_stack.rb
+++ stacks/simple_array_stack.rb
@@ -1,16 +1,40 @@
-class stack
-    attr_reader :capacity
+require 'pry'
 
-    def initialize
+class Stack
+    attr_reader :capacity, :array
+
+    def initialize(capacity)
+        @array = []
+        @capacity = capacity
     end
 
-    def push
+    def push(data)
+        if size == capacity
+            "Stack Overflow"
+        else
+            array.push(data)
+        end
     end
 
     def pop
+        if size < 1
+            "Stack Underflow"
+        else
+            array.pop()
+        end
     end
 
     def size
+        array.length
     end
     
+    def print_stack
+        array.each do |a| 
+            puts a
+            puts " "
+        end
+    end
 end
+
+stack = Stack.new(5)
+pry.binding
